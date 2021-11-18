cd2aabccb89cc0320bc528d054f04d16c142e45d
TankingItOneStep <49975142+TankingItOneStep@users.noreply.github.com>
2021-11-18 08:54:32 -0600

    Update Messages.py

--- src/data/Messages.py
+++ src/data/Messages.py
@@ -2,4 +2,5 @@ GUIDE_ALL = "You can check out the megathread with guides & resources here\n" \
             "General Guide: \n➡ <https://redd.it/mijeye>\n\n" \
             "Comprehensive Pet Guide: \n➡ <https://redd.it/migde3>\n\n" \
             "Detailed Seasons Guide: \n➡ <https://redd.it/mijcse>\n\n" \
+            "Endgame Video Guide: \n➡<https://www.youtube.com/watch?v=KhVEBiNDz1M>\n\n" \
             "You may also want to visit <https://www.reddit.com/r/AlmostAHero/wiki/index> 😃"
